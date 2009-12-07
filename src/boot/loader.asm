8e220d7ff30b6adf7f2708b5257a2bf4279c33cb
Nick Markwell <GuitarMaster92@gmail.com>
2009-12-07 00:28:42 -0500

    Print sortakinda works, gets to line 53 (`jmp _hlt`)

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -5,17 +5,49 @@ global _start
 
 jmp _start
 
-[bits 32]
+[bits 16]
+
+[section .data]
+MA20Enabling:       db   'Enabling A20.            '
+MA20EnablingEnd:
+MA20AlreadyEnabled: db   'A20 Previously enabled.  '
+MA20AlreadyEnabledEnd:
 
 [section .text]
-
-; Enable a20 if need be
+%include "src/boot/print.asm"
+%include "src/boot/a20_test.asm"
 %include "src/boot/a20_enable.asm"
 
-; Test if a20 has been set up
-%include "src/boot/a20_test.asm"
 _start:
   cli
+
+	mov bx, 000Fh			; Page 0, color attribute 15 (white) for the int 10 calls below
+	mov cx, 1			; We will write 1 char
+	xor dx, dx			; Start at top-left corner
+	mov ds, dx			; Ensure ds = 0 (to let us load the message)
+	cld				; Ensure direction flag is cleared for LODSB
+
+  ; Test if a20 has been set up
+  call check_a20
+
+  cmp ax, 1         ; Check if ax was set to 1 by check_a20 (see a20_test.asm)
+  je a20_enabled    ; Don't enable a20 if it was already done
+
+
+a20_not_enabled:
+  ; Enable a20 if need be
+  call enable_A20
+  jmp a20_enabled 
+
+a20_enabled:
+  push MA20EnablingEnd
+  push MA20Enabling
+  call print
+  add esp, 8
+  ;mov ax, 0xb800
+  ;mov ds, ax
+  ;mov byte [ds:0x0], 'a'
+  ;mov byte [ds:0x1], 0x1f
 
   ; Insert crap to load the kernel here
   jmp _hlt
