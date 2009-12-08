e08f05f3c01cf563b86eb66952b5a5e303981f6d
Martin Brandenburg <brandenburgmk@gmail.com>
2009-12-07 19:41:21 -0500

    Add support for 2nd stage.

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 AS=nasm
 ASFLAGS=-fbin
 
-OBJDIR="obj/boot"
-SRCDIR="src/boot"
+OBJDIR="obj/mbr"
+SRCDIR="src/mbr"
 LOADERNAME="loader"
+S2OBJDIR="obj/stage2"
+S2SRCDIR="src/stage2"
+STAGE2NAME="stage2"
 ISOFILE="cd.iso"
 
 if [ "-g" == "$1" ]; then
@@ -24,11 +27,13 @@ function _ {
 	$@
 }
 
-mkdir -p obj/boot
+mkdir -p ${OBJDIR} ${S2OBJDIR}
 
 _ ${AS} ${ASDEBUG} ${ASFLAGS} -o ${OBJDIR}/${LOADERNAME}.o ${SRCDIR}/${LOADERNAME}.asm
+_ ${AS} ${ASDEBUG} ${ASFLAGS} -o ${S2OBJDIR}/${STAGE2NAME}.o ${S2SRCDIR}/${STAGE2NAME}.asm
 
 _ dd bs=1024 count=1440 if=/dev/zero of=fdd.img
 _ mkfs.msdos fdd.img
-_ dd bs=512 count=1 if=obj/boot/loader.o of=fdd.img conv=notrunc
+_ dd bs=512 count=1 if=obj/mbr/loader.o of=fdd.img conv=notrunc
+_ dd bs=512 count=2 seek=1 if=obj/stage2/stage2.o of=fdd.img conv=notrunc
 
