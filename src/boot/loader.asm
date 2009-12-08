7977dd8c12f2d8449f2613528a5e6647ff97bdc2
Martin Brandenburg <brandenburgmk@gmail.com>
2009-12-07 19:17:33 -0500

    Reformatted some code. Rewrite print.

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -1,90 +1,46 @@
-; Yay http://www.osdever.net/tutorials/ch01.php and 
-; http://wiki.osdev.org/A20
-
-org 7C00h
+bits 16
+org 0x7c00
 
 global _start
 
-jmp _start
+section .text
 
-[bits 16]
+	mov ax, 0x0
+	mov ds, ax
+	jmp 0x0:_start
 
-[section .data]
-MA20Enabling:       db   'Enabling A20.            '
-MA20EnablingEnd:
-MA20AlreadyEnabled: db   'A20 Previously enabled.  '
-MA20AlreadyEnabledEnd:
-
-[section .text]
-%include "src/boot/print.asm"
+%include 'src/boot/print.asm'
 %include "src/boot/a20_test.asm"
 %include "src/boot/a20_enable.asm"
 
+_halt:
+	cli
+	.halt:
+		hlt
+		jmp .halt
+
 _start:
-  cli
+	cli
 
-	mov bx, 000Fh			; Page 0, color attribute 15 (white) for the int 10 calls below
-	mov cx, 1			    ; We will write 1 char
-	xor dx, dx			  ; Start at top-left corner
-	mov ds, dx			  ; Ensure ds = 0 (to let us load the message)
-	cld				        ; Ensure direction flag is cleared for LODSB
+	mov si, IDString
+	call print
 
-  ; Test if a20 has been set up
-  call check_a20
+	call check_a20
+	cmp ax, 1
+	je .a20_enabled
 
-  cmp ax, 1         ; Check if ax was set to 1 by check_a20 (see a20_test.asm)
-  je a20_enabled    ; Don't enable a20 if it was already done
+	.a20_not_enabled:
+		call enable_A20
 
+	.a20_enabled:
+		mov si, A20String
+		call print
+		
 
-a20_not_enabled:
-  ; Enable a20 if need be
-  call enable_A20
-  jmp a20_enabled 
+	jmp _halt
 
-a20_enabled:
-  push MA20EnablingEnd
-  push MA20Enabling
-  call print
-  add esp, 8
+A20String db 'A20',13,10,0
+IDString db 'Semplice Stage 1',13,10,0
 
-  ; Insert crap to load the kernel here
-  jmp _hlt
+dw 0xaa55
 
-  ; Set up the stack
-  mov ax,0x100
-  mov ss,ax
-  mov sp,0x200
-
-  ; Set up GDT Register
-  GDTR:
-    GDTsize DW 0x10 ; limit
-    GDTbase DD 0x50 ; base address
-
-  ; Load GDT
-  lgdt[GDTR]
-
-  ; Something with GDT?
-NULL_SEL:
-  DD 0
-  DD 0
-  
-  ; Switch to real mode
-  mov eax, cr0
-  or al, 1
-  mov cr0, eax
-  
-  ; Jump! *thud*
-  ;jmp 08h:<what the heck goes here>
-
-_start_kernel:
-  ;call kmain ; Run the kernel
-  jmp _hlt    ; In case the universe implodes and _hlt in fact is not next...
-
-_hlt:
-  cli
-  hlt
-
-; Enter protected mode
-
-TIMES 510-($-$$) DB 0
-SIGNATURE DW 0xAA55
