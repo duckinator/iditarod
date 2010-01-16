f1a2152ba31e3a646e7a2e0fd00adbd294c66807
Martin Brandenburg <brandenburgmk@gmail.com>
2010-01-16 16:46:31 -0500

    Added some code to switch to pmode, but it doesn't work.

--- src/mbr/loader.asm
+++ src/mbr/loader.asm
@@ -52,13 +52,67 @@ _start:
 	mov dh, 0x0
 	mov dl, 0x0
 	int 0x13
+	
+	; Reset drives
+	mov ah, 0x0
+	mov dl, 0x0
+	int 0x13
 
-	jmp 0x7e00
+	; Read from drive
+	mov ax, 0x2000
+	mov es, ax
+	mov bx, 0x0
+	mov ah, 0x02
+	mov al, 0x9
+	mov ch, 0x0
+	mov cl, 0x3
+	mov dh, 0x0
+	mov dl, 0x0
+	int 0x13
+
+	; Load a GDT
+	xor ax, ax
+	mov ds, ax
+	lgdt [gdt_desc]
+
+	; Switch to protected mode
+	mov eax, cr0
+	or eax, 1
+	mov cr0, eax
+
+	jmp 0x08:0x7e00
 
 	jmp _halt
 
 A20String db 'A20',13,10,0
 IDString db 'Semplice Stage 1',13,10,0
+
+gdt:
+
+gdt_null:
+	dq 0
+
+gdt_code:
+	dw 0xffff
+	dw 0
+	db 0
+	db 10011010b
+	db 11001111b
+	db 0
+
+gdt_data:
+	dw 0xffff
+	dw 0
+	db 0
+	db 10010010b
+	db 11001111b
+	db 0
+
+gdt_end:
+
+gdt_desc:
+	db gdt_end - gdt - 1
+	dw gdt
 
 times 510-($-$$) db 0x0
 dw 0xaa55
