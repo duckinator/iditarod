5f96c53d3a87c6aee77b7df405776b55f9e47955
Martin Brandenburg <brandenburgmk@gmail.com>
2010-01-16 16:46:31 -0500

    Added some code to switch to pmode, but it doesn't work.

--- src/stage2/stage2.asm
+++ src/stage2/stage2.asm
@@ -1,7 +1,18 @@
-bits 16
+bits 32
+org 0x7e00
+
+mov eax, 0x10
+mov ds, eax
+mov es, eax
+mov fs, eax
+mov gs, eax
+mov ss, eax
 
 mov ax, 0xb800
 mov ds, ax
 mov byte [ds:0x0], 'a'
 mov byte [ds:0x1], 0x2f
-jmp $
+
+mov esp, 0x90000
+
+mov byte [ds:0xb8000], 'b'
