e6300209ba0e8f9af437d858d68e815aacadbee0
Marie Markwell <me@marie.so>
2014-10-27 06:24:13 -0400

    Remove old build system.

--- makefloppy.sh
+++
@@ -1,5 +0,0 @@
-dd bs=1024 count=1440 if=/dev/zero of=fdd.img
-mkfs.msdos fdd.img
-dd bs=512 count=1 if=obj/loader of=fdd.img conv=notrunc
-dd bs=512 count=2 seek=1 if=obj/semplice/stage2 of=fdd.img conv=notrunc
-
