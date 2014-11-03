d3f707af85025927983ec43253c53ad0f8ab6843
Ellen Marie Dash <me@duckie.co>
2014-11-03 00:18:25 -0500

    Misc cleanup; remove A20 test (enabling it twice will hopefully be inconsequential).

--- src/mbr/loader.asm
+++ src/mbr/loader.asm
@@ -5,120 +5,115 @@ global _start
 
 section .text
 
-	jmp 0x0:_start
+jmp 0x0:_start
 
 %include 'src/mbr/bios_parameter_block.asm'
 
 %include 'src/mbr/print.asm'
-%include "src/mbr/a20_test.asm"
 %include "src/mbr/a20_enable.asm"
 
 _halt:
-	cli
-	.halt:
-		hlt
-		jmp .halt
+  cli
+  .halt:
+    hlt
+    jmp .halt
 
 _start:
-	cli
+  cli
 
-	mov ax, 0x0
-	mov ds, ax
+  mov ax, 0x0
+  mov ds, ax
 
-	mov si, IDString
-	call print
+  print IDString
 
-	call check_a20
-	cmp ax, 1
-	je .a20_enabled
+  print A20Enabling
+  call enable_a20
+  print Done
 
-	.a20_not_enabled:
-		call enable_A20
+  print Stage2Loading
 
-	.a20_enabled:
-		mov si, A20String
-		call print
+  ; Reset drives
+  mov ah, 0x0
+  mov dl, 0x0
+  int 0x13
 
-	; Reset drives
-	mov ah, 0x0
-	mov dl, 0x0
-	int 0x13
+  ; Read from drive
+  mov ax, 0x0
+  mov es, ax
+  mov bx, 0x7e00
+  mov ah, 0x02
+  mov al, 0x1
+  mov ch, 0x0
+  mov cl, 0x2
+  mov dh, 0x0
+  mov dl, 0x0
+  int 0x13
+  
+  ; Reset drives
+  mov ah, 0x0
+  mov dl, 0x0
+  int 0x13
 
-	; Read from drive
-	mov ax, 0x0
-	mov es, ax
-	mov bx, 0x7e00
-	mov ah, 0x02
-	mov al, 0x1
-	mov ch, 0x0
-	mov cl, 0x2
-	mov dh, 0x0
-	mov dl, 0x0
-	int 0x13
-	
-	; Reset drives
-	mov ah, 0x0
-	mov dl, 0x0
-	int 0x13
+  ; Read from drive
+  mov ax, 0x2000
+  mov es, ax
+  mov bx, 0x0
+  mov ah, 0x02
+  mov al, 0x9
+  mov ch, 0x0
+  mov cl, 0x3
+  mov dh, 0x0
+  mov dl, 0x0
+  int 0x13
 
-	; Read from drive
-	mov ax, 0x2000
-	mov es, ax
-	mov bx, 0x0
-	mov ah, 0x02
-	mov al, 0x9
-	mov ch, 0x0
-	mov cl, 0x3
-	mov dh, 0x0
-	mov dl, 0x0
-	int 0x13
+  cli
 
-	cli
+  ; Load a GDT
+  xor ax, ax
+  mov ds, ax
+  lgdt [gdt_desc]
 
-	; Load a GDT
-	xor ax, ax
-	mov ds, ax
-	lgdt [gdt_desc]
+  ; Switch to protected mode
+  mov eax, cr0
+  or eax, 1
+  mov cr0, eax
 
-	; Switch to protected mode
-	mov eax, cr0
-	or eax, 1
-	mov cr0, eax
+  jmp 0x08:0x7e00
 
-	jmp 0x08:0x7e00
+  jmp _halt
 
-	jmp _halt
-
-A20String db 'A20',13,10,0
-IDString db 'Semplice Stage 1',13,10,0
+IDString      db `Semplice Stage 1\r\n`, 0
+A20Enabling   db 'Enabling A20... ', 0
+Stage2Loading db `Loading Stage 2...`, 0
+Done          db `Done.\r\n`, 0
 
 gdt:
 
 gdt_null:
-	dd 0
-	dd 0
+  dd 0
+  dd 0
 
 gdt_code:
-	dw 0xffff
-	dw 0
-	db 0
-	db 10011010b
-	db 11001111b
-	db 0
+  dw 0xffff
+  dw 0
+  db 0
+  db 10011010b
+  db 11001111b
+  db 0
 
 gdt_data:
-	dw 0xffff
-	dw 0
-	db 0
-	db 10010010b
-	db 11001111b
-	db 0
+  dw 0xffff
+  dw 0
+  db 0
+  db 10010010b
+  db 11001111b
+  db 0
 
 gdt_end:
 
 gdt_desc:
-	dw gdt_end - gdt - 1
-	dd gdt
+  dw gdt_end - gdt - 1
+  dd gdt
 
 times 510-($-$$) db 0x0
 dw 0xaa55
