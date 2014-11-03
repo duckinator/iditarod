c5490375d0aa12a2cf0e64e1a92a5298a3529449
Marie Markwell <me@marie.so>
2014-11-03 00:18:25 -0500

    Misc cleanup; remove A20 test (enabling it twice will hopefully be inconsequential).

--- src/mbr/print.asm
+++ src/mbr/print.asm
@@ -1,13 +1,18 @@
 bits 16
 
-print:
-	mov ah, 0xe
-	mov bh, 0
-	.type:
-		lodsb
-		or al, al
-		jz .done
-		int 0x10
-		jmp .type
-	.done:
-		ret
+_print:
+  mov ah, 0xe
+  mov bh, 0
+  .type:
+    lodsb
+    or al, al
+    jz .done
+    int 0x10
+    jmp .type
+  .done:
+    ret
+
+%macro print 1
+  mov si, %1
+  call _print
+%endmacro
