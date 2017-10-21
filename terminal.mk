4f82bf84339bf0a50d3092baf6d62f19267936d7
Ellen Marie Dash <me@duckie.co>
2017-10-20 23:19:51 -0400

    Remove now-unused terminal.mk.

--- terminal.mk
+++
@@ -1,7 +0,0 @@
-COLOR_GREEN  = "\\033[0\;32m" # Set console color to green
-COLOR_RED    = "\\033[0\;31m" # Set console color to red
-COLOR_BLUE   = "\\033[0\;34m" # Set console color to blue
-COLOR_RESET  = "\\033[0m"    # Set console color to previously used one
-
-STATUS       = printf "${COLOR_GREEN}\\b["$1"]${COLOR_RESET} $(dir $(2))${COLOR_RED}\\b$(notdir $(2))${COLOR_RESET}\r\n"
-
