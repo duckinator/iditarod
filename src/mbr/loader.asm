615e9931fe4cefffb259a56cf59962d22b15a12c
Ellen Marie Dash <me@duckie.co>
2017-10-22 23:37:20 -0400

    woooooo

--- src/mbr/loader.asm
+++ src/mbr/loader.asm
@@ -1,22 +1,37 @@
 bits 16
 org 0x7c00
 
+%define STAGE2_ADDR 0x7e00
+
 section .text
 
 jmp 0x0:_start
 
-%include 'src/mbr/bios_parameter_block.asm'
-%include 'src/mbr/load_stage2.asm'
+BIOSParameterBlock:
+  dw 0x000B ; BytesPerSector
+  db 0x000D ; SectorsPerCluster
+  dw 0x000E ; ReservedSectors
+  db 0x0010 ; FatCopies
+  dw 0x0011 ; RootDirEntries
+  dw 0x0013 ; NumSectors
+  db 0x0015 ; MediaType
+  dw 0x0016 ; SectorsPerFAT
+  dw 0x0018 ; SectorsPerTrack
+  dw 0x001A ; NumberOfHeads
+  dd 0x001C ; HiddenSectors
+  dd 0x0020 ; SectorsBig
 
 _start:
   cli
 
   ; Attempt to load stage2 from hard disk.
-  call load_stage2
+  mov si, data_address_packet
+  mov ah, 0x42  ; ?
+  mov dl, 0x80  ; Drive number. (?)
+  int 0x13      ; Tell the BIOS to load it.
 
   ; Jump to stage2 if it's been loaded successfully.
-  ; 0x7e00 needs to match src/mbr/load_stage2.asm.
-  jnc 0x7e00
+  jnc STAGE2_ADDR
 
   ; If we get here, we've failed to load stage2, so print the failure message.
   mov si, FailureMessage
@@ -35,6 +50,14 @@ halt:
   jmp halt
 
 FailureMessage  db `Can't load Semplice Stage 2.\r\n`, 0
+
+data_address_packet:
+  db  16            ; Length in bytes.
+  db  0             ; Always zero.
+  dw  16            ; Number of blocks to read. (I think?)
+  dw  STAGE2_ADDR   ; Memory buffer destination address.
+  dw  0             ; Memory page.
+  dd  1, 0          ; LBA to be read.
 
 times 510-($-$$) db 0x0
 dw 0xaa55
