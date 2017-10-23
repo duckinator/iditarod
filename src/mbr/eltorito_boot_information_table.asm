336c6d1538ce14af677a13d8a5240dedfa62604b
Ellen Marie Dash <me@duckie.co>
2017-10-22 22:00:49 -0400

    Make Boot Information Table always be at the correct offset.

--- src/mbr/eltorito_boot_information_table.asm
+++ src/mbr/eltorito_boot_information_table.asm
@@ -1,5 +1,10 @@
 section .text
 
+; Boot Information Table is always at offset 8.
+; Causes an assembler error if it's preceded by more than 8 bytes worth of
+; code.
+times 8-($-$$) db 0
+
 ; Boot Information Table
 PrimaryVolumeDescriptor  resd  1    ; LBA of the Primary Volume Descriptor
 BootFileLocation         resd  1    ; LBA of the Boot File
