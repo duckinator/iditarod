ee4c180d8ebce5f737ab37d642728f98ac3b789e
Ellen Marie Dash <me@duckie.co>
2017-10-26 03:46:29 -0400

    lol fuck everything

--- src/eltorito/eltorito.asm
+++ src/eltorito/eltorito.asm
@@ -10,16 +10,26 @@ jmp 0x0:_start
 ; generates more than 8 bytes of machine code.
 times 8-($-$$) db 0
 
-; Boot Information Table
-PrimaryVolumeDescriptor  resd  1    ; LBA of the Primary Volume Descriptor
-BootFileLocation         resd  1    ; LBA of the Boot File
-BootFileLength           resd  1    ; Length of the boot file in bytes
-Checksum                 resd  1    ; 32 bit checksum
-Reserved                 resb  40   ; Reserved 'for future standardization'
+; Boot Information Table.
+; I think this is automagically populated, but I'm not sure.
+PrimaryVolumeDescriptor  dd    0    ; Space for LBA of Primary Volume Descriptor
+BootFileLocation         dd    0    ; Space for LBA of the Boot File
+BootFileLength           dd    0    ; Space for length of the boot file in bytes
+Checksum                 dd    0    ; Space for 32 bit checksum
+Reserved                 times 40 db 0 ; Reserved 'for future standardization'
+
+; Things that need to be manually populated.
 
 DriveNumber              resb  1    ; Reserve space for the drive number.
 DiskPacket               resb  32   ; Reserve space to shove bullshit in.
-BytesPerSector           resw  1    ; Reserve space for bytes per sector.
+BytesPerSector           times 1  dw 0    ; Reserve space for bytes per sector.
+SectorDescriptor         times 32 db 0  ; Reserve space for sector location.
+
+LoadingMessage          db `Attempting to load Stage 2...\r\n`, 0
+SectorReadErrorMessage  db `Failed to read sector.\r\n`, 0
+GenericFailureMessage   db `Can't load Stage 2.\r\n`, 0
+
+CDSignature     db `CD001`
 
 _start:
   cli
@@ -52,48 +62,77 @@ _start:
   ; Save the boot disk number.
   mov [DriveNumber], dl
 
-  ; Get the number of bytes in a sector.
-  ; Usually 2048 bytes.
-  mov word [DiskPacket], 0x1a
-  mov ah, 0x48
+  mov ax, DiskPacket + 0x18 ; ??? what the fuck?
+  mov [BytesPerSector], ax
+
+  ; stage2 is in /boot on the disk. Information about the root directory
+  ; provided by PrimaryVolumeDescriptor.
+  ; I think that should include information about subdirectories?
+  ; Look at that for a directory named "BOOT", then look in that.
+  ;
+  ; I have no idea what I'm doing.
+
+  ; Build data packet to request the BIOS load a sector.
+    mov byte [DiskPacket], 0x10
+
+    mov word [DiskPacket + 2], 1 ; i don't know
+
+    ; Write the destination address
+    mov word [DiskPacket + 4], bx
+    mov word [DiskPacket + 6], es
+
+    ; Write the logical sector to load
+    mov long [DiskPacket + 8], eax
+    mov long [DiskPacket + 12], 0
+
+.read_sector:
+  ; Read a single sector into memory.
+  mov ah, 0x42
   mov al, 0
   mov si, DiskPacket
   mov dl, DriveNumber
   int 0x13
-  jc assume_2k_sector
+  jnc .read_sector_success
 
-  mov ax, DiskPacket + 0x18 ; ??? what the fuck?
-  mov [BytesPerSector], ax
-  jmp load_sector
+  mov si, SectorReadErrorMessage
+  call print
+  jmp halt
 
-assume_2k_sector:
-  ; Assume 2KB sector.
-  ; Should probably print an error, but I don't really care right now.
-  mov word [BytesPerSector], 0x0800
+.read_sector_success:
+  ; Update the sector to load.
+  inc long [DiskPacket + 8]
 
-load_sector:
-  ; stage2 is in the root directory of the disk.
-  ; I have no idea what I'm doing.
-  ; Good luck.
+  ; Update destination address.
+  ; Instead of incrementing the offset, we increase the segment.
+  ; Honestly, I'm just copypasta-ing this bullshit.
+  mov word ax, BytesPerSector
+  shr word ax, 4
+  add word [DiskPacket + 6], ax
+  loop .read_sector
 
-fail:
-  ; If we get here, we've failed to load stage2, so print the failure message.
-  mov si, FailureMessage
+  ret
 
+print:
   mov ah, 0xe
   .print_character:
     lodsb
     cmp al, 0
-    jz halt
+    jz .done
     int 0x10
     jmp .print_character
+  .done:
+    ret
+
+fail:
+  ; If we get here, we've failed to load stage2, but don't know why,
+  ; so print a generic failure message.
+  mov si, GenericFailureMessage
+  call print
 
 halt:
   cli
   hlt
   jmp halt
-
-FailureMessage  db `Can't load Semplice Stage 2.\r\n`, 0
 
 times 510-($-$$) db 0x0
 dw 0xaa55
